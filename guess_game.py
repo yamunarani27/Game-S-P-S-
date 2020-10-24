d1962a15251c9f4ae89dbf17c7d90bfc5fcb56c7
yamunarani <63499482+yamunarani27@users.noreply.github.com>
2020-10-24 17:36:47 +0530

    Add files via upload
    Guess a number generated by system in three guesses.

---
+++ guess_game.py
@@ -0,0 +1,23 @@
+import random
+
+player1=random.randint(1,100)
+trial_count=1
+
+print("Let us start the game")
+print(player1)
+
+while(trial_count<=3):
+
+    player2=int(input("Enter a number:"))
+    if player1>player2:
+        print("value is  higher than your guess")
+    elif player1<player2:
+        print("value is less than your guess")
+    else:
+        print("your guess is right")
+        break
+        
+    trial_count+=1
+    
+else:
+    print("your trials are over")
