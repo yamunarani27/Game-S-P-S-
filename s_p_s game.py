2dc998e351d00f78e7298c7a6e7753c5d96c1f0d
yamunarani <63499482+yamunarani27@users.noreply.github.com>
2022-07-21 21:02:39 +0200

    Update s_p_s game.py
    reduced complexity of code

--- s_p_s game.py
+++ s_p_s game.py
@@ -1,28 +1,21 @@
 import random
 
-computer=["stone","paper","scissor"]
 player1_score=player2_score=0
 play_count=1
 
 print("Let us start the game")
+while(play_count<=3):
 
-while(play_count<=10):
-
-    player1=input("Enter ur choice:")
-
-    player2=random.choice(computer)
-
-    print("player2's choice is "+player2)
-
-    if ((player1=="stone") and (player2=="scissor")) or ((player1=="paper") and (player2=="stone")) or ((player1=="scissor") and (player2=="paper")):
-        player1_score +=1
-    elif ((player1=="paper") and (player2=="scissor")) or ((player1=="scissor") and (player2=="stone")) or ((player1=="stone") and (player2=="paper")):
-        player2_score +=1
-    else:
+    player1=input("Enter ur choice stone/paper/scissor:")
+    player2=random.choice(["stone","paper","scissor"])
+    print(player2)
+    if player1==player2:
         print("Both are in tie")
-
+    elif ((player1=="stone") and (player2=="scissor")) or ((player1=="paper") and (player2=="stone")) or ((player1=="scissor") and (player2=="paper")):
+        player1_score +=1
+    else:
+        player2_score +=1        
     play_count+=1
     
-
-print("player1's score is:",player1_score)
-print("player2's score is:",player2_score)
+print("player1's score :",player1_score)
+print("player2's score :",player2_score)
