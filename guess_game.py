b40d561c19a2432ccee5c570cfe0d1293d2a7aac
yamunarani <63499482+yamunarani27@users.noreply.github.com>
2022-07-21 16:17:09 +0200

    Update guess_game.py
    Tried with for loop and reduction in number of lines of code

--- guess_game.py
+++ guess_game.py
@@ -1,23 +1,32 @@
 import random
 
-player1=random.randint(1,100)
-trial_count=1
+secret_number=random.randint(1,11)
 
-print("Let us start the game")
-print(player1)
-
-while(trial_count<=3):
-
-    player2=int(input("Enter a number from 1-99:"))
-    if player1>player2:
-        print("value is  higher than your guess")
-    elif player1<player2:
-        print("value is less than your guess")
-    else:
-        print("your guess is right")
+print("Welcome to the guess game!!!!You have 3 chances to guess the right number")
+for i in range(1,4):
+    number=int(input("enter your guess between 1 to 10:"))
+    if i==3:
+       print("your guessess are over,the secret number is"+str(secret_number))
+       break
+    if (number == secret_number):
+        print("Congrats,your guess is right!!!!")
         break
-        
-    trial_count+=1
+    elif(number<secret_number):
+        print("Try with a number greater than the guessed number")
+    else:
+        print("Try with a number lesser than the guessed number")
+
     
-else:
-    print("your trials are over")
+# for i in range (1,4):
+#     number=int(input("enter your guess between 1 to 10:"))
+#     if (number == secret_number):
+#         print("Congrats,your guess is right!!!!")
+#         break
+#     else:
+#         if i==3:
+#             print("sorry,your guessess are wrong.The secret number is "+str(secret_number))
+#         elif(number<secret_number):
+#             print("Try with a number greater than the guessed number")
+#         else:
+#             print("Try with a number lesser than the guessed number")
+
